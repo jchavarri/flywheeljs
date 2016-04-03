a921e55bd063aaa1c49919310f629b51a240c182
Javier Chávarri <javier.chavarri@gmail.com>
2016-04-03 16:13:21 +0200

    Updating nodejs example to include geocoding using google maps api

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -1,10 +1,14 @@
-const flywheel = require('flywheeljs');
 const program = require('commander');
 const chalk = require('chalk');
+const axios = require('axios');
+// const flywheel = require('flywheeljs');
+const flywheel = require('../../dist/flywheel');
+const mergeObjects = require('./utils').mergeObjects;
+const readline = require('readline');
 
 program
-  .description('A nodejs tool to manage cabs from Flywheel')
-  .version('0.0.1')
+  .description('A nodejs tool to manage ordering taxis using the Flywheel API')
+  .version('0.0.2')
 
 program
   .command('account')
@@ -34,39 +38,84 @@ program
   .description('request a new ride at a given location')
   .option('-e, --email <email>', '[required] The email of the user to authenticate as')
   .option('-p, --password <password>', '[required] The user\'s password')
-  .option('--latitude <latitude>', '[required] The latitude to request at (in degrees). Usage: --latitude=\'37.2241822\'')
-  .option('--longitude <longitude>', '[required] The latitude to request at (in degrees). Usage: --longitude=\'-121.2572231\'')
+  .option('-a, --address <address>', '[required] The address to request the taxi at. Usage: --address \'22 Estate Ct, South San Francisco, CA\'')
   .option('-t, --tip <tip>', '[optional] The tip for the ride, in cents. For example, for the minimum amount ($5) the param should be \'--tip 500\'')
   .option('-n, --notes <notes>', '[optional] Some notes to add to the ride request. Use it with quotes: --notes \'Park below the white building\'')
+  .option('-s, --skip', '[optional] Skip address confirmation step')
   .action(function(options){
     const tip = options.tip || 500;
     const notes = options.notes || '';
     var authToken = undefined;
     var passenger = undefined;
-    if (options.email && options.password && options.latitude && options.longitude) {
-      flywheel.login({
-        email: options.email,
-        password: options.password
+
+    if (options.email && options.password && options.address) {
+      var addressData = {}; // Will fill this object with google geocoding results
+      const googleUrl = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(options.address);
+      axios.get(googleUrl)
+      .then(function (response) {
+        const results = response.data.results;
+        if (!results || results.length === 0) {
+          return Promise.reject('We couldn\'t find any addresses corresponding to \'' + options.address + '\'. Use a comma separated format including the state, for example: --address \'22 Estate Ct, South San Francisco, CA\'');
+        }
+        const googleData = results[0];
+        locationData = {
+          pickUpLat: googleData.geometry.location.lat,
+          pickUpLon: googleData.geometry.location.lng,
+          address: googleData.address_components
+            .filter((el) => (el.types.indexOf('street_number') !== -1 || el.types.indexOf('route') !== -1))
+            .reduce((prev, next) => prev + (prev.length === 0 ? '' : ' ') + next.short_name, ''),
+          state: googleData.address_components
+            .filter((el) => el.types.indexOf('administrative_area_level_1') !== -1)
+            .reduce((prev, next) => next.short_name, ''),
+          country: googleData.address_components
+            .filter((el) => el.types.indexOf('country') !== -1)
+            .reduce((prev, next) => next.long_name, ''),
+          city: googleData.address_components
+            .filter((el) => el.types.indexOf('locality') !== -1)
+            .reduce((prev, next) => next.long_name, '')
+        };
+        if (!options.skip) {
+          return new Promise((resolve, reject) => {
+            const rl = readline.createInterface({
+              input: process.stdin,
+              output: process.stdout
+            });
+            rl.question('\nEverything ready! A taxi will be ordered to pick you up at this address:\n' +
+              JSON.stringify(locationData, null, '  ') +
+              '\n\nAre you sure you want to continue? (Y/n)',
+              (yesno) => {
+                rl.close();
+                if (yesno.length === 0 || yesno.toLowerCase() === 'y' || yesno.toLowerCase() === 'yes')
+                  resolve();
+                else
+                  reject('The request was canceled by user');
+              });
+          });
+        }
+      })
+      .then(function() {
+        return flywheel.login({
+          email: options.email,
+          password: options.password
+        });
       })
       .then(function(response) {
         authToken = response.auth_token;
         passenger = response.passenger;
         return flywheel.applicationContext({
           authToken: authToken,
-          latitude: options.latitude,
-          longitude: options.longitude
+          latitude: locationData.pickUpLat,
+          longitude: locationData.pickUpLon
         });
       })
       .then(function(response) {
         if (!response.service_availabilities || response.service_availabilities.length === 0) {
-          throw new Error('There\'s no service available at those coordinates. Please check with another latitude and/or longitude');
+          return Promise.reject('Flywheel is not available at this address. Please check with another address, or try to add more information to it (postal code, state, city)');
         }
         if (!passenger.payment_instruments || passenger.payment_instruments.length === 0) {
-          throw new Error('There are no payment instruments linked to this account. Please add a payment instrument from the Flywheel mobile app');
+          return Promise.reject('There are no payment instruments linked to this account. Please add a payment instrument from the Flywheel mobile app');
         }
-        return flywheel.createRide({
-          pickUpLat: options.latitude,
-          pickUpLon: options.longitude,
+        return flywheel.createRide(mergeObjects(locationData, {
           passenger: {
             name: passenger.name,
             phone: passenger.telephone
@@ -76,10 +125,10 @@ program
           tip: tip,
           notes: notes,
           authToken: authToken
-        });
+        }));
       })
       .then(function(response) {
-        console.log("🚕  Your request has been created! Your ride id is: " + response.id);
+        console.log(chalk.green('👍  Your request has been created! Your ride id is: ' + response.id));
         var counter = 0;
         const timeout = 120;
         const timeBetweenTrials = 2;
@@ -99,11 +148,11 @@ program
                 return response;
               }
               else if (!response.someRandom && counter < maxTrials) {
-                console.log("⏳ Waiting for a driver to accept the ride. Remaining attempts: " + (maxTrials - counter));
+                console.log(chalk.green('⏳  Waiting for a driver to accept the ride. Remaining attempts: ' + (maxTrials - counter)));
                 return retry();
               }
               else {
-                throw new Error('Too much time went by without a driver accepting the ride. Please cancel your ride using the \'cancel\' command or open the Flywheel app on your phone to handle this ride from there.');
+                return Promise.reject('Too much time went by without a driver accepting the ride. Please cancel your ride using the \'cancel\' command or open the Flywheel app on your phone to handle this ride from there.');
               }
             });
           });
@@ -112,7 +161,7 @@ program
         return retry();
       })
       .then(function(response) {
-        console.log('🔝🔝 Yay!! Your request has been accepted. These are the details:\n', response);
+        console.log(chalk.green('🚕  Yay!! Your request has been accepted. These are the details of your ride and driver:\nid: ' + response.id + '\nride data: ', JSON.stringify(response.hail, null, '  ')));
       })
       .catch(function(error) {
         console.log(chalk.red('There was an error: ⚠️\n', JSON.stringify(error, null, '  ')));
@@ -150,7 +199,7 @@ var generateRideFunction = function(name, description, flywheelFunction) {
       });
     }
     else {
-      console.log(chalk.yellow('Missing params <email>, <password>, <rideId>. Use -h or --help to get some usage information'));
+      console.log(chalk.yellow('Some of these params are missing: <email>, <password>, <rideId>. Use -h or --help to get some usage information'));
     }
   });
 }
